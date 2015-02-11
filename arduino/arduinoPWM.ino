4b549e66563e9d01624058d4f1b7eb56a252495a
Steven Sloboda <sloboste@umich.edu>
2015-02-11 17:31:12 -0500

    Worked on arduino code

--- arduino/arduinoPWM.ino
+++ arduino/arduinoPWM.ino
@@ -9,7 +9,17 @@
 const unsigned char SELF_I2C_ADDR = 0x10; // FIXME
 
 // 8-bit identification number (arbitrarily assigned) of Arduino
-const unsigned char SELF_ID = 0xAD; // FIXME
+const unsigned char SELF_CHIP_ID = 0xAD; // FIXME
+
+// Pins used on  the Arduino
+const unsigned char PIN_I2C_SDA = 0; // FIXME
+const unsigned char PIN_I2C_SCL = 0; // FIXME
+const unsigned char PIN_INTRPT = 0; // FIXME
+const unsigned char PIN_PWM_IN_S = 0; //FIXME
+const unsigned char PIN_PWM_IN_M = 0; //FIXME
+const unsigned char PIN_PWM_OUT_S = 0; //FIXME
+const unsigned char PIN_PWM_OUT_M = 0; //FIXME
+const unsigned char PIN_LED = 0; //FIXME
 
 // Defines the valid modes that the Arduino can be in 
 enum modeEnum {
@@ -30,6 +40,90 @@ enum cmdEnum {
     SET_STEER_DC      = 0x06,
     SET_MOTOR_DC      = 0x07
 };
+
+/* Interrupt driven input that allows the I2C master to wake the Arduino from 
+ * sleep mode
+ */
+volatile bool wakeup;
+
+// Mode that the Arduino is currently in
+enum modeEnum mode;
+
+// Duty cycles for steering and motor PWM signals
+unsigned char steer = 0;
+unsigned char motor = 0;
+
+
+/* Arduino setup function. Runs on device power on before the loop function is
+ * called.
+ */ 
+void setup()
+{
+    // Initialize mode
+    mode = IDLE;
+
+    // Initialize wakeup
+    wakeup = false;
+  
+    // Set i2c bus address
+    Wire.begin(SELF_I2C_ADDR); 
+    
+    // Register handlers for master read and write
+    Wire.onRecieve(masterWriteHandler);
+    Wire.onRequest(masterReadHandler);
+}
+
+/* Arduino loop function. This is the code that runs continuously on the micro-
+ * processor.
+ */
+void loop()
+{
+    // Check if we have just woken up
+    if (wakeup) {
+        // Reset wakeup flag
+        wakeup = false;
+
+        // Default to IDLE mode after being woken up
+        mode = IDLE
+
+        // Do something else FIXME
+
+    }
+
+    // Take appropriate action based on mode
+    switch (mode) {
+    case RC:
+        // Intercept PWM values from radio reciever
+        interceptPWM(steer, motor);
+        // Write these values to the motor controller and steering servo
+        setSteer(steer);
+        setMotor(motor);
+        break;
+    case RPI:
+        // Write values to motor controller and steering servo based on RPI
+        // steer and motor get values when commands are received from I2C master
+        setSteer(steer);
+        setMotor(motor);
+        break;
+    case SLEEP:
+        // Put the arduino to sleep FIXME
+        sleep_enable();
+        attachInterrupt(PIN_INTRPT, _ISR_wakeupHandler, RISING);
+        set_sleep_mode(/*look up args*/);
+        noInterrupts(); // Same as cli();
+        sleep_bod_disable(); // Disable brown out detection to save power
+        interrupts(); // Same as sei();
+        sleep_cpu();
+        // Wakes up here
+        break;
+    default: // (mode == IDLE) || (mode == undefined) 
+        // Do nothing
+        // Or maybe blink the led so we know it is idle and not fried
+        break;
+    }
+}
+
+// FUNCTIONS DEFINED BELOW
 
 /* Public: Read the PWM signals that are being sent from the radio transmitter
  *         to the car's radio receiver.
@@ -124,61 +218,7 @@ void masterWriteHandler(/*args*/)
  */
 void _ISR_wakeupHandler(/*args*/)
 {
-    // NOT YET IMPLEMENTED!!!!!!!!!
+    sleep_disable();
+    detachInterrupt(/*look up what the args are*/); // FIXME
+    wakeup = true;
 }
-
-
-/* Interrupt driven input that allows the I2C master to wake the Arduino from 
- * sleep mode
- */
-volatile bool wakeup;
-
-// Mode that the Arduino is currently in
-enum modeEnum mode;
-
-
-/* Arduino setup function. Runs on device power on before the loop function is
- * called.
- */ 
-void setup()
-{
-    // Initialize mode
-    mode = IDLE;
-
-    // Initialize wakeup
-    wakeup = false;
-  
-    // Set i2c bus address
-    Wire.begin(SELF_I2C_ADDR); 
-    
-    // Register handlers for master read and write
-    Wire.onRecieve(masterWriteHandler);
-    Wire.onRequest(masterReadHandler);
-}
-
-/* Arduino loop function. This is the code that runs continuously on the micro-
- * processor.
- */
-void loop()
-{
-    switch (mode) {
-    case RC:
-        // Intercept PWM values from radio reciever
-
-        // Write these values to the motor controller and steering servo
-
-        break;
-    case RPI:
-        // Write values to motor controller and steering servo based on RPI
-
-        break;
-    case SLEEP:
-        // Put the arduino to sleep FIXME
-
-        break;
-    default: // (mode == IDLE) || (mode == undefined) 
-        // Do nothing
-        break;
-    }
-}
-
